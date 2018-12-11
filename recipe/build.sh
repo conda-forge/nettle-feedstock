64c01d91288d7ba563f5299deb1cd26a62280df0
Marius van Niekerk <marius.v.niekerk@gmail.com>
2018-12-10 22:01:07 -0500

    Update recipe for 3.4.1\n\nRemoved direct dep on gmp as this caused failures during testing

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-./configure --prefix="${PREFIX}" --libdir="${PREFIX}/lib/" --with-lib-path="${PREFIX}/lib/"
+
+# Building with conda-forge gmp causes a strange segfault.  
+# Using mini-gmp seems to solve the issue and gnutls still works
+./configure --prefix="${PREFIX}" \
+    --libdir="${PREFIX}/lib/" \
+    --with-lib-path="${PREFIX}/lib/" \
+    --enable-mini-gmp || { cat config.log; exit 1; }
 make
 make install
 make check
